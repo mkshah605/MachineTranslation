7712a4569c613d4f6fcb59506ee5c41bb5b4f170
Mansi <mkshah605@gmail.com>
2024-09-27 14:46:42 -0400

    add docstrings and some paramters/shapes

--- tests/test_beamsearch.py
+++ tests/test_beamsearch.py
@@ -1,5 +1,14 @@
 from beamsearch import beam_search
+from unittest.mock import Mock
+
+FakeModel = Mock()
 
 def test_beamsearch():
-    beam_search()
+    """
+    - model: beamsearch implementation
+    - max_depth: we will consider at_most this many generations of a new token. If max_depth is reached, take top n beams
+    - n_beams: the number of highest probabilities that we consider at each step
 
+    """
+    samples = beam_search(FakeModel, max_depth=10, n_beams=3)
+    assert samples.shape[-1] == 3
